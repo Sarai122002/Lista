9e513ed1547908c504524acf7fb6f12d630e2975
Nathaly Rodriguez <87873901+Sarai122002@users.noreply.github.com>
2021-09-26 23:27:37 -0600

    Add files via upload

---
+++ Listas/ListaMascota/ListaMascota/FrmDatos.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace ListaMascota
+{
+    partial class FrmDatos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.cmbRaza = new System.Windows.Forms.ComboBox();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.tsmMenu = new System.Windows.Forms.ToolStrip();
+            this.tslRegistrar = new System.Windows.Forms.ToolStripLabel();
+            this.tslConsultar = new System.Windows.Forms.ToolStripLabel();
+            this.tslEliminar = new System.Windows.Forms.ToolStripLabel();
+            this.tslSalir = new System.Windows.Forms.ToolStripLabel();
+            this.tslAcercade = new System.Windows.Forms.ToolStripLabel();
+            this.erpError = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.tsmMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.erpError)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(332, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Raza";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(332, 250);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(482, 50);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(180, 25);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEdad.Location = new System.Drawing.Point(482, 246);
+            this.txtEdad.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(180, 25);
+            this.txtEdad.TabIndex = 5;
+            // 
+            // cmbRaza
+            // 
+            this.cmbRaza.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbRaza.FormattingEnabled = true;
+            this.cmbRaza.Items.AddRange(new object[] {
+            "",
+            "Pitbull",
+            "Doberman",
+            "Show show",
+            "Pug",
+            "Dalmata",
+            "Chihuaha"});
+            this.cmbRaza.Location = new System.Drawing.Point(482, 145);
+            this.cmbRaza.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbRaza.Name = "cmbRaza";
+            this.cmbRaza.Size = new System.Drawing.Size(180, 26);
+            this.cmbRaza.TabIndex = 6;
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.BackgroundColor = System.Drawing.Color.PaleTurquoise;
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.GridColor = System.Drawing.Color.MistyRose;
+            this.dgvDatos.Location = new System.Drawing.Point(336, 302);
+            this.dgvDatos.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(650, 229);
+            this.dgvDatos.TabIndex = 7;
+            // 
+            // tsmMenu
+            // 
+            this.tsmMenu.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.tsmMenu.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tsmMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslRegistrar,
+            this.tslConsultar,
+            this.tslEliminar,
+            this.tslSalir,
+            this.tslAcercade});
+            this.tsmMenu.Location = new System.Drawing.Point(0, 0);
+            this.tsmMenu.Name = "tsmMenu";
+            this.tsmMenu.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.tsmMenu.Size = new System.Drawing.Size(1142, 25);
+            this.tsmMenu.TabIndex = 8;
+            this.tsmMenu.Text = "toolStrip1";
+            // 
+            // tslRegistrar
+            // 
+            this.tslRegistrar.Name = "tslRegistrar";
+            this.tslRegistrar.Size = new System.Drawing.Size(69, 22);
+            this.tslRegistrar.Text = "Registrar";
+            this.tslRegistrar.Click += new System.EventHandler(this.tslRegistrar_Click);
+            // 
+            // tslConsultar
+            // 
+            this.tslConsultar.Name = "tslConsultar";
+            this.tslConsultar.Size = new System.Drawing.Size(71, 22);
+            this.tslConsultar.Text = "Consultar";
+            this.tslConsultar.Click += new System.EventHandler(this.tslConsultar_Click);
+            // 
+            // tslEliminar
+            // 
+            this.tslEliminar.Name = "tslEliminar";
+            this.tslEliminar.Size = new System.Drawing.Size(66, 22);
+            this.tslEliminar.Text = "Eliminar";
+            this.tslEliminar.Click += new System.EventHandler(this.tslEliminar_Click);
+            // 
+            // tslSalir
+            // 
+            this.tslSalir.Name = "tslSalir";
+            this.tslSalir.Size = new System.Drawing.Size(39, 22);
+            this.tslSalir.Text = "Salir";
+            this.tslSalir.Click += new System.EventHandler(this.tslSalir_Click);
+            // 
+            // tslAcercade
+            // 
+            this.tslAcercade.Name = "tslAcercade";
+            this.tslAcercade.Size = new System.Drawing.Size(87, 22);
+            this.tslAcercade.Text = "Acerca de...";
+            // 
+            // erpError
+            // 
+            this.erpError.ContainerControl = this;
+            // 
+            // FrmDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ListaMascota.Properties.Resources.Animals;
+            this.ClientSize = new System.Drawing.Size(1142, 564);
+            this.Controls.Add(this.tsmMenu);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.cmbRaza);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmDatos";
+            this.Text = "Lista Mascotas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.tsmMenu.ResumeLayout(false);
+            this.tsmMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.erpError)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.ComboBox cmbRaza;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.ToolStrip tsmMenu;
+        private System.Windows.Forms.ToolStripLabel tslRegistrar;
+        private System.Windows.Forms.ToolStripLabel tslConsultar;
+        private System.Windows.Forms.ToolStripLabel tslEliminar;
+        private System.Windows.Forms.ToolStripLabel tslSalir;
+        private System.Windows.Forms.ToolStripLabel tslAcercade;
+        private System.Windows.Forms.ErrorProvider erpError;
+    }
+}
+
---
+++ Listas/ListaMascota/ListaMascota/FrmDatos.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaMascota
+{
+    public partial class FrmDatos : Form
+    {
+        List<ClsLista> MiLista = new List<ClsLista>();
+
+        public FrmDatos()
+        {
+            InitializeComponent();
+            tslConsultar.Enabled = false;
+            tslEliminar.Enabled = false;
+
+        }
+        private void tslRegistrar_Click(object sender, EventArgs e)
+        {
+            if (ValidarNombre() == false)
+            {
+                return;
+            }
+            if (ValidarRaza() == false)
+            {
+                return;
+            }
+            if (ValidarEdad() == false)
+            {
+                return;
+            }
+            if (Existe(txtNombre.Text))
+            {
+                erpError.SetError(txtNombre, "La mascota con ese nombre ya esta registrada");
+                txtNombre.Focus();
+                LimpiarCrontroles();
+                return;
+            }
+            erpError.SetError(txtNombre, "");
+
+          //creamos el objeto de la clas lista
+            ClsLista miMascota = new ClsLista();
+            miMascota.Nombre = txtNombre.Text;
+            miMascota.Raza = cmbRaza.SelectedItem.ToString();
+            miMascota.Edad = int.Parse(txtEdad.Text);
+            MiLista.Add(miMascota);
+            dgvDatos.DataSource = null;
+            dgvDatos.DataSource = MiLista;
+            LimpiarCrontroles();
+            txtNombre.Focus();
+            tslConsultar.Enabled = true;
+
+
+        }
+
+        // Metodo que valida que no ingrsen con el mismo nombre
+        private bool Existe(string nombre)
+        {
+            foreach (ClsLista miMascota in MiLista) 
+            {
+                if (miMascota.Nombre==nombre)
+                {
+                    return true;
+                }
+            }
+            return false; 
+        }
+
+        private bool ValidarEdad()
+        {
+            int Edad;
+            if (!int.TryParse(txtEdad.Text, out Edad) || txtEdad.Text == "")
+            {
+                erpError.SetError(txtEdad, "Debe ingresar un valor numerico para la edad");
+                txtEdad.Clear();
+                txtEdad.Focus();
+                return false;
+
+            }
+            else
+            {
+                erpError.SetError(txtEdad, "");
+                return true;
+            }
+        }
+
+        private bool ValidarNombre()
+        {
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                erpError.SetError(txtNombre, "Debe ingresar un nombre");
+                return false;
+            }
+            else
+            {
+                erpError.SetError(txtNombre, "");
+                return true;
+            }
+        }
+
+        //Validar la raza
+        private bool ValidarRaza()
+        {
+            if (string.IsNullOrEmpty(cmbRaza.Text))
+            {
+                erpError.SetError(cmbRaza, "Debe Seleccionar una raza");
+                return false;
+            }
+            else
+            {
+                erpError.SetError(cmbRaza, "");
+                return true;
+            }
+        }
+        //Metodo para limpiar los controles
+        private void LimpiarCrontroles()
+        {
+            txtNombre.Clear();
+            txtEdad.Clear();
+            cmbRaza.SelectedIndex = 0;
+
+
+        }
+
+        private void tslSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Evento de la opcion consultar
+        private void tslConsultar_Click(object sender, EventArgs e)
+        {
+            if (ValidarNombre() == false)
+            {
+                return;
+            }
+            ClsLista miMascota = GetMascota(txtNombre.Text);
+            if (miMascota == null)
+            {
+                erpError.SetError(txtNombre, "La mascota no se encuentra registrada en la lista");
+                LimpiarCrontroles();
+                txtNombre.Focus();
+                return;
+            }
+            else
+            {
+                erpError.SetError(txtNombre, "");
+                txtNombre.Text = miMascota.Nombre;
+                cmbRaza.SelectedItem = miMascota.Raza;
+                txtEdad.Text = miMascota.Edad.ToString();
+                tslEliminar.Enabled = true;
+
+
+            }
+
+        }
+
+        //Metodo obtener o consultar mascota
+        private ClsLista GetMascota(string nombre)
+        {
+            return MiLista.Find(mascota => mascota.Nombre.Contains(nombre));
+        }
+
+        //Evento de la opcion eliminar
+        private void tslEliminar_Click(object sender, EventArgs e)
+        {
+
+            if (txtEdad.Text == "")
+            {
+                erpError.SetError(txtNombre, "Debe  consultar la mascota a eliminar");
+                LimpiarCrontroles();
+                txtNombre.Focus();
+                tslEliminar.Enabled = false;
+                return;
+            }
+            else
+            {
+                erpError.SetError(txtNombre, "");
+                DialogResult Respuesta = MessageBox.Show("Esta seguro de eliminar el registro?", "Confirmacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+                if (Respuesta == DialogResult.Yes)
+                {
+                    foreach (ClsLista miMascota in MiLista)
+                    {
+                        if (miMascota.Nombre == txtNombre.Text) 
+                        {
+                            MiLista.Remove(miMascota);
+                            break;
+                        }
+                    }
+                    LimpiarCrontroles();
+                    dgvDatos.DataSource = null;
+                    dgvDatos.DataSource = MiLista;
+                }
+
+            }
+        }
+    }
+}
+
